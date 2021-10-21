2599d66ddeb28766006966ec98ab3043326c7a87
nowosad20p <91263440+nowosad20p@users.noreply.github.com>
2021-10-21 12:17:28 +0200

    second player

--- Projekt JS/main.js
+++ Projekt JS/main.js
@@ -7,72 +7,106 @@ let borders=[];
 
 
 
-var upKey;
-var rightKey;
-var downKey;
-var leftKey;
+var upKey1;
+var rightKey1;
+var downKey1;
+var leftKey1;
+var upKey2;
+var rightKey2;
+var downKey2;
+var leftKey2;
+
 
 window.onload=function(){
 canvas=document.getElementById("gameCnv");
 context=canvas.getContext("2d");
 
-player = new Player(100,400);
-for(let i=0;i<6;i++){
-    borders.push(new Border(0,710,1000,10,1));
-}
+canvas2=document.getElementById("gameCnv2");
+context2=canvas2.getContext("2d");
 
-    borders.push(new Border(100,610,100,100,1));
+player = new Player(10,10,0);
+player2=new Player(30,30,1)
 
 gameLoop=setInterval(step,1000/30);
 
 context.fillStyle="lightGray"
-context.fillRect(0,0,1280,720);
-
+context.fillRect(0,0,1280,360);
+context2.fillStyle="darkGray"
+context2.fillRect(0,0,1280,360);
 setupInputs();
+setupLvl1();
 }
 function step(){
     player.step();
-    
+    player2.step();
     draw();
 }
 function draw(){
    
     context.fillStyle="lightGray";
-context.fillRect(0,0,1280,720);
+context.fillRect(0,0,1280,360);
+context2.fillStyle="lightGray";
+context2.fillRect(0,0,1280,360);
 player.draw();
+player2.draw();
 for(let i=0;i<borders.length;i++){
     borders[i].draw();
 }
 }
 function setupInputs(){
     document.addEventListener("keydown",function(event){
-      if(event.key==="w"||event.key==="ArrowUp"){
-          upKey=true;
+      if(event.key==="w"){
+          upKey1=true;
       }
-      if(event.key==="a"||event.key==="ArrowLeft"){
-        leftKey=true;
+      if(event.key==="a"){
+        leftKey1=true;
     }
-    if(event.key==="s"||event.key==="ArrowDown"){
-        downKey=true;
+    if(event.key==="s"){
+        downKey1=true;
     }
-    if(event.key==="d"||event.key==="ArrowRight"){
-        rightKey=true;
+    if(event.key==="d"){
+        rightKey1=true;
     }
+    if(event.key==="ArrowUp"){
+        upKey2=true;
+    }
+    if(event.key==="ArrowLeft"){
+      leftKey2=true;
+  }
+  if(event.key==="ArrowDown"){
+      downKey2=true;
+  }
+  if(event.key==="ArrowRight"){
+      rightKey2=true;
+  }
     });
     document.addEventListener("keyup",function(event){
-        if(event.key==="w"||event.key==="ArrowUp"){
-            upKey=false;
+        if(event.key==="w"){
+            upKey1=false;
         }
-        if(event.key==="a"||event.key==="ArrowLeft"){
-          leftKey=false;
+        if(event.key==="a"){
+          leftKey1=false;
       }
-      if(event.key==="s"||event.key==="ArrowDown"){
-          downKey=false;
+      if(event.key==="s"){
+          downKey1=false;
       }
-      if(event.key==="d"||event.key==="ArrowRight"){
-          rightKey=false;
+      if(event.key==="d"){
+          rightKey1=false;
       }
+      if(event.key==="ArrowUp"){
+        upKey2=false;
+    }
+    if(event.key==="ArrowLeft"){
+      leftKey2=false;
+  }
+  if(event.key==="ArrowDown"){
+      downKey2=false;
+  }
+  if(event.key==="ArrowRight"){
+      rightKey2=false;
+  }
       });
+
 }
 function checkIntersection(r1,r2){
     if(r1.x>r2.x+r2.width){
@@ -86,4 +120,19 @@ function checkIntersection(r1,r2){
     }else{
         return true;
     }
+}
+function setupLvl1(){
+    for(let i=0;i<6;i++){
+    
+        borders.push(new Border(0,350,1000,10,1,0));
+    }
+    
+        borders.push(new Border(100,210,100,100,1,0));
+        for(let i=0;i<6;i++){
+    
+            borders.push(new Border(0,350,1000,10,1,1));
+        }
+        
+            borders.push(new Border(100,210,100,100,1,1));
+        
 }
--- Projekt JS/player.js
+++ Projekt JS/player.js
@@ -1,45 +1,69 @@
-function Player(x,y){
+function Player(x,y,type){
     this.x=x;
     this.y=y;
     this.xspeed=0;
     this.yspeed=0;
     this.friction=0.6;
-   
+   this.type=type;
     this.maxSpeed=10;
     this.width=50;
     this.height=50;
     this.active=true;
     this.step=function(){
     if(this.active){
-        if(!leftKey&&!rightKey||leftKey&&rightKey){
+        if(this.type==0){
+
+        
+        if(!leftKey1 && !rightKey1||leftKey1 && rightKey1){
             this.xspeed*=this.friction;
-        }else if(rightKey){
+        }else if(rightKey1){
               this.xspeed++;
-        }else if(leftKey){
+        }else if(leftKey1){
             this.xspeed--;
         }
       
-        if(upKey){
+        if(upKey1){
             if(this.yspeed==0){
                 this.yspeed=-35;
             }
-                
+            
             
         }
-        this.yspeed+=5;
+    }
+    if(type==1){
 
+        
+        if(!leftKey2 && !rightKey2||leftKey2 && rightKey2){
+            this.xspeed*=this.friction;
+        }else if(rightKey2){
+              this.xspeed++;
+        }else if(leftKey2){
+            this.xspeed--;
+        }
+      
+        if(upKey2){
+            if(this.yspeed==0){
+                this.yspeed=-35;
+            }
+            
+            
+        }
+    }
+        
+    this.yspeed+=5;
         if(this.xspeed>this.maxSpeed){
             this.xspeed=this.maxSpeed;
         }
         if(this.xspeed<-this.maxSpeed){
             this.xspeed=-this.maxSpeed;
         }
-        // if(this.yspeed>this.maxSpeed){
-        //     this.yspeed=this.maxSpeed;
-        //  }
-        //  if(this.yspeed<-this.maxSpeed){
-        //      this.yspeed=-this.maxSpeed;
-        //  }        
+        if(this.yspeed>this.maxSpeed){
+            this.yspeed=this.maxSpeed;
+         }
+         if(this.yspeed<-this.maxSpeed){
+             this.yspeed=-this.maxSpeed;
+         }      
+          
        
         // if(this.xspeed>0){
         //     this.xspeed=Math.floor(this.xspeed);
@@ -86,8 +110,15 @@ function Player(x,y){
 
     }
     this.draw=function(){
-      context.fillStyle="black";
-      context.fillRect(this.x,this.y,this.width,this.height);
+        if(type==0){
+            context.fillStyle="black";
+            context.fillRect(this.x,this.y,this.width,this.height);
+        }
+        if(type==1){
+            context2.fillStyle="yellow";
+            context2.fillRect(this.x,this.y,this.width,this.height);
+        }
+     
     }
     
 }
